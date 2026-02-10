410ada5020207a99b1c0796e6c850a93edefda92
Joseph Lubandi <lubandi1234@gmail.com>
2026-02-10 19:43:01 +0200

    chore(shortener): add custom models to django admin dashboard

--- module-6-7-8-9/shortener/admin.py
+++ module-6-7-8-9/shortener/admin.py
@@ -1,1 +1,22 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import URL, Click, Tag
+
+
+@admin.register(URL)
+class URLAdmin(admin.ModelAdmin):
+    list_display = ["short_code", "original_url", "owner", "click_count", "created_at"]
+    list_filter = ["created_at", "is_active"]
+    search_fields = ["short_code", "original_url", "title"]
+    readonly_fields = ["click_count"]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Click)
+class ClickAdmin(admin.ModelAdmin):
+    list_display = ["url", "clicked_at", "ip_address", "country"]
+    list_filter = ["clicked_at", "country"]
