12f06d992ef1ee3ea5243542052076d9556e0615
Joseph Lubandi <lubandi1234@gmail.com>
2026-02-10 19:49:22 +0200

    feat(shortener): update url serializer with new fields

--- module-6-7-8-9/shortener/serializers.py
+++ module-6-7-8-9/shortener/serializers.py
@@ -6,5 +6,15 @@ from .models import URL
 class URLSerializer(serializers.ModelSerializer):
     class Meta:
         model = URL
-        fields = ["original_url", "short_code", "created_at"]
-        read_only_fields = ["short_code", "created_at"]
+        fields = [
+            "original_url",
+            "short_code",
+            "custom_alias",
+            "tags",
+            "expires_at",
+            "click_count",
+            "is_active",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = ["short_code", "click_count", "created_at", "updated_at"]
