6977e997f0b9366e09d55858b0c2eb05bdfd90eb
Joseph Lubandi <lubandi1234@gmail.com>
2026-02-10 16:07:57 +0200

    feat(core): implement custom User model and reset migrations

--- module-6-7-8-9/config/settings.py
+++ module-6-7-8-9/config/settings.py
@@ -48,10 +48,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # Third-party apps
     "rest_framework",
     "drf_spectacular",
-    # Local apps
     "core",
     "shortener",
     "api",
@@ -140,6 +138,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "core.User"
 
 # DRF Configuration
 REST_FRAMEWORK = {
--- module-6-7-8-9/core/admin.py
+++ module-6-7-8-9/core/admin.py
@@ -1,1 +1,14 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ["email", "username", "tier", "is_premium", "is_staff"]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("tier", "is_premium")}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("email", "tier", "is_premium")}),
+    )
