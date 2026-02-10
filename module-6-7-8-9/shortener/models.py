d7b98eade83ba20d9821e2f389e72f7b7e61b436
Joseph Lubandi <lubandi1234@gmail.com>
2026-02-10 16:12:05 +0200

    feat(shortener): implement Tag model

--- module-6-7-8-9/shortener/models.py
+++ module-6-7-8-9/shortener/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class URL(models.Model):
     original_url = models.URLField(max_length=500)
     short_code = models.CharField(max_length=10, unique=True)
