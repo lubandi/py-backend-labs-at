223c1182628d56b669c18734d07d0eb1a1518e06
Joseph Lubandi <lubandi1234@gmail.com>
2026-02-11 19:56:47 +0200

    feat(auth): Implement JWT authentication and user registration

--- module-6-7-8-9/api/urls.py
+++ module-6-7-8-9/api/urls.py
@@ -1,6 +1,11 @@
+from core.views import UserRegistrationView
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from shortener.views import URLCreateView
 
 urlpatterns = [
+    path("auth/register/", UserRegistrationView.as_view(), name="user-register"),
+    path("auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("urls/", URLCreateView.as_view(), name="url-create"),
 ]
--- module-6-7-8-9/core/views.py
+++ module-6-7-8-9/core/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from .serializers import UserRegistrationSerializer
+
+
+class UserRegistrationView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(
+            {"message": "User registered successfully."}, status=status.HTTP_201_CREATED
+        )
